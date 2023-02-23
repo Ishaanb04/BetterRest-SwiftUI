ad4f5e1040ddaba6096f6c773fb0daa2ad01d093
Ishaan Bhasin <ibhasin@terpmail.umd.edu>
2023-02-23 17:33:38 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Ishaan Bhasin on 2/23/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
